9105ccdd149b009b95932597057e1fc0cce79dda
Balram Chavan <balram.chavan@hexad.de>
2018-07-12 06:50:50 +0200

    1. Show Lat-Long on mouse move 2. Show Lat-Long on mouse click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,24 @@ export class AppComponent {
   map: any;
 
   ngOnInit() {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
     this.map = new ol.Map({
       target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
       layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
@@ -24,6 +40,16 @@ export class AppComponent {
         center: ol.proj.fromLonLat([73.8567, 18.5204]),
         zoom: 8
       })
+    });
+
+    this.map.on('click', function (args) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+      
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
     });
   }
 
